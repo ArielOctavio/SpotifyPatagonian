b1eb65cb18ad4f5689b2277c66b82ed4a12680de
Ariel <arieldalfeo@gmail.com>
2020-09-15 18:44:29 -0300

    Style

--- SpotifyMVC/Controllers/HomeController.cs
+++ SpotifyMVC/Controllers/HomeController.cs
@@ -9,26 +9,7 @@ namespace SpotifyMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-       
+              
 
         public ActionResult SearchTrackSpotify()
         {
--- SpotifyMVC/Models/SpotifySearch.cs
+++ SpotifyMVC/Models/SpotifySearch.cs
@@ -1,113 +1,124 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SpotifyMVC.Models
 {
 
-        public class ExternalUrls
-        {
-            public string spotify { get; set; }
-        }
-
-        public class Artist
-        {
-            public ExternalUrls external_urls { get; set; }
-            public string href { get; set; }
-            public string id { get; set; }
-            public string name { get; set; }
-            public string type { get; set; }
-            public string uri { get; set; }
-        }
-
-        public class ExternalUrls2
-        {
-            public string spotify { get; set; }
-        }
-
-        public class Image
-        {
-            public int height { get; set; }
-            public string url { get; set; }
-            public int width { get; set; }
-        }
-
-        public class Album
-        {
-            public string album_type { get; set; }
-            public List<Artist> artists { get; set; }
-            public List<string> available_markets { get; set; }
-            public ExternalUrls2 external_urls { get; set; }
-            public string href { get; set; }
-            public string id { get; set; }
-            public List<Image> images { get; set; }
-            public string name { get; set; }
-            public string release_date { get; set; }
-            public string release_date_precision { get; set; }
-            public int total_tracks { get; set; }
-            public string type { get; set; }
-            public string uri { get; set; }
-        }
-
-        public class ExternalUrls3
-        {
-            public string spotify { get; set; }
-        }
-
-        public class Artist2
-        {
-            public ExternalUrls3 external_urls { get; set; }
-            public string href { get; set; }
-            public string id { get; set; }
-            public string name { get; set; }
-            public string type { get; set; }
-            public string uri { get; set; }
-        }
-
-        public class ExternalIds
-        {
-            public string isrc { get; set; }
-        }
-
-        public class ExternalUrls4
-        {
-            public string spotify { get; set; }
-        }
-
-        public class Item
-        {
-            public Album album { get; set; }
-            public List<Artist2> artists { get; set; }
-            public List<string> available_markets { get; set; }
-            public int disc_number { get; set; }
-            public int duration_ms { get; set; }
-            public bool @explicit { get; set; }
-            public ExternalIds external_ids { get; set; }
-            public ExternalUrls4 external_urls { get; set; }
-            public string href { get; set; }
-            public string id { get; set; }
-            public bool is_local { get; set; }
-            public string name { get; set; }
-            public int popularity { get; set; }
-            public string preview_url { get; set; }
-            public int track_number { get; set; }
-            public string type { get; set; }
-            public string uri { get; set; }
-        }
-
-        public class Tracks
-        {
-            public string href { get; set; }
-            public List<Item> items { get; set; }
-            public int limit { get; set; }
-            public string next { get; set; }
-            public int offset { get; set; }
-            public object previous { get; set; }
-            public int total { get; set; }
-        }
-
-        public class Root
-        {
-            public Tracks tracks { get; set; }
-        }
-
-
+    public class ExternalUrls
+    {
+        public string spotify { get; set; }
     }
+
+    public class Artist
+    {
+        public ExternalUrls external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class ExternalUrls2
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Image
+    {
+        public int height { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Album
+    {
+        public string album_type { get; set; }
+        public List<Artist> artists { get; set; }
+        public List<string> available_markets { get; set; }
+        public ExternalUrls2 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public List<Image> images { get; set; }
+        public string name { get; set; }
+        public string release_date { get; set; }
+        public string release_date_precision { get; set; }
+        public int total_tracks { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class ExternalUrls3
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Artist2
+    {
+        public ExternalUrls3 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class ExternalIds
+    {
+        public string isrc { get; set; }
+    }
+
+    public class ExternalUrls4
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Item
+    {
+        public Album album { get; set; }
+        [Display(Name = "Artista")]
+        public List<Artist2> artists { get; set; }
+        public List<string> available_markets { get; set; }
+
+        [Display(Name = "#")]
+        public int disc_number { get; set; }
+        [Display(Name = "Duración ms")]
+        public int duration_ms { get; set; }
+        public bool @explicit { get; set; }
+        public ExternalIds external_ids { get; set; }
+        public ExternalUrls4 external_urls { get; set; }
+        [Display(Name = "URL link")]
+        public string href { get; set; }
+        public string id { get; set; }
+        public bool is_local { get; set; }
+        [Display(Name = "Nombre")]
+        public string name { get; set; }
+        [Display(Name = "Popularidad")]
+        public int popularity { get; set; }
+        [Display(Name = "Preview")]
+        public string preview_url { get; set; }
+        public int track_number { get; set; }
+        public string type { get; set; }
+        [Display(Name = "Abrir en Spotify")]
+        public string uri { get; set; }
+    }
+
+    public class Tracks
+    {
+        public string href { get; set; }
+        public List<Item> items { get; set; }
+        public int limit { get; set; }
+        public string next { get; set; }
+        public int offset { get; set; }
+        public object previous { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Root
+    {
+        public Tracks tracks { get; set; }
+    }
+
+
+}
